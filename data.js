c3aa9e572b25c775ef7af668a9498765c828f1d1
Mintae Lee <lee.mintae@codeimmersives.com>
2019-03-27 14:06:15 -0400

    added data into data.js

--- data.js
+++ data.js
@@ -1,3 +1,154 @@
-const data = [
+const data = [ 
+    {
+        lastName: 'Bell',
+        firstName: 'Jordan',
+        position: 'Forward',
+        height: 81,
+        weight: 224,
+        team: 'Golden State Warriors'
+    },
     
+    {
+        lastName: 'Bogut',
+        firstName: 'Andrew',
+        position: 'Center',
+        height: 84,
+        weight: 260,
+        team: 'Golden State Warriors'
+    },
+
+    {
+        lastName: 'Cook',
+        firstName: 'Quinn',
+        position: 'Guard',
+        height: 74,
+        weight: 179,
+        team: 'Golden State Warriors'
+    },
+    
+    {
+        lastName: 'Cousins',
+        firstName: 'DeMarcus',
+        position: 'Center',
+        height: 83,
+        weight: 270,
+        team: 'Golden State Warriors'
+    },
+
+    {
+        lastName: 'Curry',
+        firstName: 'Stephen',
+        position: 'Guard',
+        height: 75,
+        weight: 190,
+        team: 'Golden State Warriors'
+    },
+
+    {
+        lastName: 'Derrickson',
+        firstName: 'Marcus',
+        position: 'Forward',
+        height: 79,
+        weight: 249,
+        team: 'Golden State Warriors'
+    },
+
+    {
+        lastName: 'Durant',
+        firstName: 'Kevin',
+        position: 'Forward',
+        height: 81,
+        weight: 240,
+        team: 'Golden State Warriors'
+    },
+
+    {
+        lastName: 'Evans',
+        firstName: 'Jacob',
+        position: 'Guard',
+        height: 78,
+        weight: 210,
+        team: 'Golden State Warriors'
+    },
+
+    {
+        lastName: 'Green',
+        firstName: 'Draymond',
+        position: 'Forward',
+        height: 79,
+        weight: 230,
+        team: 'Golden State Warriors'
+    },
+
+    {
+        lastName: 'Iguodala',
+        firstName: 'Andre',
+        position: 'Guard',
+        height: 78,
+        weight: 215,
+        team: 'Golden State Warriors'
+    },
+    
+    {
+        lastName: 'Jerebko',
+        firstName: 'Jonas',
+        position: 'Forward',
+        height: 82,
+        weight: 231,
+        team: 'Golden State Warriors'
+    },
+
+    {
+        lastName: 'Jones',
+        firstName: 'Damian',
+        position: 'Center',
+        height: 84,
+        weight: 245,
+        team: 'Golden State Warriors'
+    },
+
+    {
+        lastName: 'Lee',
+        firstName: 'Damion',
+        position: 'Guard',
+        height: 78,
+        weight: 210,
+        team: 'Golden State Warriors'
+    },
+
+    {
+        lastName: 'Livingston',
+        firstName: 'Shaun',
+        position: 'Guard',
+        height: 79,
+        weight: 192,
+        team: 'Golden State Warriors'
+    },
+
+    {
+        lastName: 'Looney',
+        firstName: 'Kevon',
+        position: 'Center',
+        height: 81,
+        weight: 220,
+        team: 'Golden State Warriors'
+    },
+
+    {
+        lastName: 'McKinnie',
+        firstName: 'Alfonzo',
+        position: 'Forward',
+        height: 78,
+        weight: 215,
+        team: 'Golden State Warriors'
+    },
+
+    {
+        lastName: 'Thompson',
+        firstName: 'Klay',
+        position: 'Guard',
+        height: 79,
+        weight: 215,
+        team: 'Golden State Warriors'
+    },
 ]
--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 window.onload = init;
+
+function init() {
+    document.querySelector('#search')
+        .addEventListener('click', searchValue);
+
+    document.querySelector('#display-all')
+        .addEventListener('click', displayAll);
+
+}
+
+function displayAll(event){
+    
+}
+
+function searchValue(event){
+
+}
